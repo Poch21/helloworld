7f070fcee0b7089a0a9197c19b54da7a252c1d07
Poche <manimanito.184@gmail.com>
2021-05-05 18:53:28 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+console.log('hello world');
+/* Console.log(' '); mostrará lo que le pongas en los parentesis en la consola 
+Ctrl + shift + i /abre la consola del navegador
+*/
+var mostrarEnLaConsola = 'Ctrl + shift + i';
+console.log(mostrarEnLaConsola);
+
+// var people = {
+//     name: 'Miki',
+//     ptt: 'y no te mando un saludo',
+//  };
+
+// const btn = document.getElementById('botoncito'); 
+// btn.addEventListener('click', () => {
+//     alert('Te dije que no me tocaras')
+// })
+// btn.addEventListener('click', () => {
+//     text1.style.color = 'white';
+//     text1.style.background = 'black';
+//     text1.innerText = `Yo soy ${people.name}, ${people.ptt} porque tocaste el boton. Abre la consola con: ${mostrarEnLaConsola}`;
+// })
+// const text1 = document.getElementById('text1');
